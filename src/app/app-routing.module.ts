8cfa8453e0d9c70b14c5b706135af5ab3febdcd9
Sungwon Ryu <sungwon@syllabussoftware.org>
2018-12-03 19:28:01 -0500

    Add routes in app-routing.module.ts
    . Add "", "login", "crewboard" and "main" routes in the root path
    . Add "", "current-assignments", "past-assignments" and "manage-database" routes under "main" path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './auth/login/login.component';
+import { CrewboardComponent } from './crewboard/crewboard.component';
+import { MainComponent } from './main/main.component';
+import { CurrentAssignmentsComponent } from './current-assignments/current-assignments.component';
+import { PastAssignmentsComponent } from './past-assignments/past-assignments.component';
+import { ManageDatabaseComponent } from './manage-database/manage-database.component';
+
+const defaultPath = 'main/current-assignments';
+
+const routes: Routes = [
+  { path: '', redirectTo: defaultPath, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'crewboard', component: CrewboardComponent },
+  { path: 'main', component: MainComponent, children: [
+    { path: '', redirectTo: defaultPath, pathMatch: 'full' },
+    { path: 'current-assignments', component: CurrentAssignmentsComponent },
+    { path: 'past-assignments', component: PastAssignmentsComponent },
+    { path: 'manage-database', component: ManageDatabaseComponent }
+  ] },
+  { path: '**', redirectTo: defaultPath }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
