770fcc0bae210b1b5f2e3b6215b5d4a9e91b3a01
Sungwon Ryu <sungwon@syllabussoftware.org>
2018-12-07 16:30:32 -0500

    Import MatInputModule, MatPaginatorModule, MatSortModule, MatTableModule and MatIcon in material.module.ts

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 
+import {
+  MatIconModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTableModule
+} from '@angular/material';
+
+
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [
+    MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule
+  ],
+  exports: [
+    MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule
+  ]
 })
 export class MaterialModule { }
