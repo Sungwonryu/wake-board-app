76ef9afcae11e5442335f7fd901a9d0bace65e58
Sungwon Ryu <sungwon@syllabussoftware.org>
2018-12-11 19:07:17 -0500

    Add table-wrapper.component.ts in shared/tables/table-wrapper/
    . Add @Input titlebarView, tableTitle, tableView, tableColumns, dataType, data and @Output onModifyTable
    . Add HArray and filterValue
    . Add applyFilter(), modify(), addEntry() and duplicateAllEntries()

--- src/app/shared/tables/table-wrapper/table-wrapper.component.ts
+++ src/app/shared/tables/table-wrapper/table-wrapper.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { HArray } from '../../common/h-array';
+
+import { TableActionData, TableColumn, TableView, TitlebarView } from '../table.model';
 
 @Component({
   selector: 'app-table-wrapper',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableWrapperComponent implements OnInit {
 
+  HArray = HArray;
+
+  // table titlebar view settings
+  @Input() titlebarView: TitlebarView = {};
+  @Input() tableTitle: string = '';
+
+  // table column and view settings
+  @Input() tableView: TableView = {};
+  @Input() tableColumns: TableColumn[] = [];
+
+  // table data and filter settings
+  @Input() dataType: string = '';
+  @Input() data: any[] = [];
+  // @Input() filterValue: string = '';
+
+  filterValue: string = '';
+
+  @Output() onModifyTable = new EventEmitter<TableActionData>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  applyFilter(filterValue: string) {
+    this.filterValue = filterValue;
+  }
+
+  modify(tableActionData: TableActionData) {
+    this.onModifyTable.emit(tableActionData);
+  }
+
+  addEntry() {
+    const tableActionData: TableActionData = { tableAction: 'add', dataType: this.dataType };
+    this.onModifyTable.emit(tableActionData);
+  }
+
+  duplicateAllEntries() {
+    const tableActionData: TableActionData = { tableAction: 'duplicateAll', dataType: this.dataType, entries: this.data };
+    this.onModifyTable.emit(tableActionData);
   }
 
 }
