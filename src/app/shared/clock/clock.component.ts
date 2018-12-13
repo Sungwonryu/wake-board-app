4a1a949c344a4ec652a92a45ac9e9febd63cd793
Sungwon Ryu <sungwon@syllabussoftware.org>
2018-12-13 15:17:46 -0500

    Add the codes for ClockComponent in shared/clock/
    . Update this.clock using setInterval() in 500 ms delay interval
    . Add span.clock and span.ordinal-suffix using DatePipe and  OrdinalSuffixPipe

--- src/app/shared/clock/clock.component.ts
+++ src/app/shared/clock/clock.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.scss']
 })
-export class ClockComponent implements OnInit {
+export class ClockComponent implements OnDestroy, OnInit {
+
+  @Input() format: string = '';
+  @Input() ordinalSuffix: boolean = false;
+
+  clock: Date = null;
+  intervalId: any = null;
+  intervalDelay = 500; // 500 ms
 
   constructor() { }
 
   ngOnInit() {
+    this.clock = new Date();
+    this.intervalId = setInterval(() => {
+      this.clock = new Date();
+    }, this.intervalDelay);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 
 }
