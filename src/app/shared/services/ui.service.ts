fe93575d940b92e5d86e3e983251c4c74d7cdaa6
Sungwon Ryu <sungwon@syllabussoftware.org>
2018-12-27 17:58:54 -0500

    Add UIService in shared/services/ui.service.ts
    . Add showProgressbar(), hideProgressbar(), showSnackbar() and showApiResponseMessage()

---
+++ src/app/shared/services/ui.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { MatSnackBar } from '@angular/material';
+
+import { ApiResponse } from '../../api-storage/api-storage.model';
+
+@Injectable()
+export class UIService {
+
+  snackbarDuration = 2000; // 2 secs
+  shortSnackbarDuration = 800; // 800 millisecs
+  actionsShowSuccessMessage = ['insert', 'update', 'delete'];
+  $loadingStateChange = new Subject<boolean>();
+
+  constructor(
+     private snackbar: MatSnackBar
+  ) { }
+
+  showProgressbar() {
+    this.$loadingStateChange.next(true);
+  }
+
+  hideProgressbar() {
+    this.$loadingStateChange.next(false);
+  }
+
+  showSnackbar({ message, action, duration }: {
+    message: string,
+    action?: string,
+    duration?: number
+  }) {
+    if (duration || typeof duration !== 'number' || !Number.isInteger(duration)) {
+      // If duration is not valid, set duration as this.snackbarDuration
+      duration = this.snackbarDuration;
+    }
+    this.snackbar.open(message, action, {
+      duration: duration
+    });
+  }
+
+  showApiResponseMessage({ apiOpts, success, error }: ApiResponse) {
+    let message: string;
+    let duration = this.snackbarDuration;
+
+    if (success) {
+      // When ApiResponse is successful
+      if (this.actionsShowSuccessMessage.indexOf(apiOpts.baseParamsObj.action) !== -1) {
+        // apiOpts.baseParamsObj.action is found in this.actionsShowSuccessMessage
+        message = `"${apiOpts.baseParamsObj.object}" - ${apiOpts.baseParamsObj.action} operation was successful.`;
+        duration = this.shortSnackbarDuration;
+      }
+    } else {
+      // When ApiResponse is not successful
+      if (error) {
+        // When there was a network failure
+        message = `Sorry, there was a http failure on "${apiOpts.baseParamsObj.object}" ${apiOpts.baseParamsObj.action} operation.
+  Please try later.`;
+      } else {
+        // When there was no network error but API operation was not successful
+        message = `Sorry, there was a problem on "${apiOpts.baseParamsObj.object}" ${apiOpts.baseParamsObj.action} operation.
+  Please try later.`;
+      }
+    }
+
+    if (message) {
+      // When message is set
+      this.showSnackbar({
+        message: message,
+        duration: duration
+      });
+    }
+  }
+}
