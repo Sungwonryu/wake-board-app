ec31872ea25c31f00c294960481f0b9168021b76
Sungwon Ryu <sungwon@syllabussoftware.org>
2018-12-27 17:13:17 -0500

    Add BaseParamsObj, ApiOpts, ApiResponse and ListUpdate interfaces in api-storage/api-storage.model.ts

---
+++ src/app/api-storage/api-storage.model.ts
@@ -0,0 +1,40 @@
+/**
+ *  ParamsObj is the interface for API query paramsObj
+*/
+export interface BaseParamsObj {
+  'action'?: string;
+  'object'?: string;
+  'user_id'?: string;
+  'assign_date'?: string;
+  'date'?: string;
+}
+
+/*  ApiOpts is the interface for the parameter of apiStorageService
+ *  ApiOpts interface uses camel case for its property names
+ *  except apiObj whose properties uses snake case
+*/
+export interface ApiOpts {
+  url?: string,
+  httpMethod?: string;
+  baseParamsObj?: BaseParamsObj;
+  apiObj?: any;
+}
+
+/**
+ *  ApiResponse is the interface for the returning objects from apiStorageService
+*/
+export interface ApiResponse {
+  apiOpts?: ApiOpts;
+  success?: boolean;
+  error?: any;
+  response?: any;
+}
+
+/**
+ *  ListUpdate is the interface for the returning objects from $listUpdate Subject
+*/
+export interface ListUpdate {
+  isUpdated?: boolean,
+  list?: any;
+  filteredList?: any;
+}
