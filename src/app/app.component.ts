eac9ca5394850df9ce1ae8a2ca9393fb0b69ae03
Sungwon Ryu <sungwon@syllabussoftware.org>
2018-12-31 13:30:55 -0500

    Add the temp codes for login() in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './auth/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(
+    private authService: AuthService
+  ) {
+
+    this.authService.login({
+      email: 'admin1@test.com',
+      password: '12345678'
+    });
+  }
+
+}
