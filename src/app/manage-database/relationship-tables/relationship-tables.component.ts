5aeb932722416f975eb788eb50385f3386502478
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-03 18:47:55 -0500

    Add headerMessage and linkList in relationship-tables.component.ts

--- src/app/manage-database/relationship-tables/relationship-tables.component.ts
+++ src/app/manage-database/relationship-tables/relationship-tables.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+// import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-relationship-tables',
   templateUrl: './relationship-tables.component.html',
   styleUrls: ['./relationship-tables.component.scss']
 })
-export class RelationshipTablesComponent implements OnInit {
+export class RelationshipTablesComponent implements OnInit, OnDestroy {
+
+  headerMessage = 'Relationships define how existing data points connect to each other. Create and manage relationships to enable the auto-fill capabilities of Wake.Board.';
+
+  tableLinkList = [
+    { routerLink: ['/main/manage-database/relationship-tables/vessel'], linkText: 'Vessels' },
+    { routerLink: ['/main/manage-database/relationship-tables/shift'], linkText: 'Shiftss' },
+    { routerLink: ['/main/manage-database/relationship-tables/crew-member'], linkText: 'Crew Members' },
+    { routerLink: ['/main/manage-database/relationship-tables/crew-swap'], linkText: 'Crew Swaps' }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+
   }
 
 }
