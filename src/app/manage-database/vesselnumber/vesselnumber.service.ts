ce21df25afd04bd969d333ac8a600dd5940ce0da
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-03 12:36:22 -0500

    Add vesselnumber.service.ts in manage-database/vesselnumber/

---
+++ src/app/manage-database/vesselnumber/vesselnumber.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { BaseDataService } from '../../api-storage/base-data.service';
+
+import { ApiStorageService } from '../../api-storage/api-storage.service';
+import { AuthService } from '../../auth/auth.service';
+import { ParamsService } from '../../shared/services/params.service';
+import { UIService } from '../../shared/services/ui.service';
+
+@Injectable()
+export class VesselnumberService extends BaseDataService {
+  // object = 'vessel_numbers';
+  object = 'vessels';
+  sortOrder = [
+    { property: 'vesselNo', isDescending: false }
+  ];
+
+  constructor(
+    protected apiStorageService: ApiStorageService,
+    protected authService: AuthService,
+    protected paramsService: ParamsService,
+    protected uiService: UIService,
+  ) {
+
+    super(
+      apiStorageService,
+      authService,
+      paramsService,
+      uiService
+    );
+    this.init();
+  }
+
+  /**
+   *  getNewList() is configured differently in each service
+   */
+  getNewList(defaultList: any[], listData: any[]) {
+    console.log(`${this.object} - listData: `, listData);
+    console.log(`${this.object} - newList: `, defaultList);
+    const newList = defaultList;
+    return newList;
+  }
+
+}
