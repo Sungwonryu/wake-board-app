989a049cedd6cd4bb8c49b207391355b4bc6f14d
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-04 18:37:38 -0500

    Add crewmember.model.ts and crewmember.service.ts in manage-database/crewmember-table/

---
+++ src/app/manage-database/crewmember-table/crewmember.model.ts
@@ -0,0 +1,19 @@
+import { BaseApiObj, BaseDataObj } from '../../api-storage/base-data';
+
+/**
+ *  CrewmemberApiObj is the interface of returning objects from API
+ *  CrewmemberApiObj interface uses underscore case for its property names
+*/
+export interface CrewmemberApiObj extends BaseApiObj {
+  'employee'?: string | null;
+  'job'?: string | null;
+}
+
+/**
+ *  Crewmember is the base interface for the objects used in the front-end
+ *  Crewmember interface uses underscore case for its property names
+*/
+export interface Crewmember extends BaseDataObj {
+  employee?: string | null;
+  job?: string | null;
+}
