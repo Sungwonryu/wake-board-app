5982bbfb5912a5f2a488b85f455768d4adbfa814
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-03 19:36:24 -0500

    Add '/main/manage-database/relationship-tables/~' paths in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,14 @@ import { CurrentAssignmentsComponent } from './assignments/current-assignments/c
 import { PastAssignmentsComponent } from './assignments/past-assignments/past-assignments.component';
 import { ManageDatabaseComponent } from './manage-database/manage-database.component';
 
+import { BasicTablesComponent } from './manage-database/basic-tables/basic-tables.component';
+import { RelationshipTablesComponent } from './manage-database/relationship-tables/relationship-tables.component';
+
+import { VesselTableComponent } from './manage-database/vessel-table/vessel-table.component';
+import { ShiftTableComponent } from './manage-database/shift-table/shift-table.component';
+import { CrewmemberTableComponent } from './manage-database/crewmember-table/crewmember-table.component';
+import { CrewswapTableComponent } from './manage-database/crewswap-table/crewswap-table.component';
+
 const defaultPath = 'main/current-assignments';
 
 const routes: Routes = [
@@ -18,7 +26,17 @@ const routes: Routes = [
     { path: '', redirectTo: defaultPath, pathMatch: 'full' },
     { path: 'current-assignments', component: CurrentAssignmentsComponent },
     { path: 'past-assignments', component: PastAssignmentsComponent },
-    { path: 'manage-database', component: ManageDatabaseComponent }
+    { path: 'manage-database', component: ManageDatabaseComponent, children: [
+      { path: '', redirectTo: '/main/manage-database/basic-tables', pathMatch: 'full' },
+      { path: 'basic-tables', component: BasicTablesComponent },
+      { path: 'relationship-tables', component: RelationshipTablesComponent, children: [
+        { path: '', redirectTo: '/main/manage-database/relationship-tables/vessel', pathMatch: 'full' },
+        { path: 'vessel', component: VesselTableComponent },
+        { path: 'shift', component: ShiftTableComponent },
+        { path: 'crew-member', component: CrewmemberTableComponent },
+        { path: 'crew-swap', component: CrewswapTableComponent }
+      ] }
+    ] }
   ] },
   { path: '**', redirectTo: defaultPath }
 ];
