06a723d3f5d682a5d895de4e99deda5c532c4c31
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-08 13:54:47 -0500

    Change api property of environment.ts from 'https://nycferry.hialabs.com/action.php' to 'https://nycferry.hialabs.com/action3.php'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   urls: {
     userData: 'assets/data/user-data.json',
-    api: 'https://nycferry.hialabs.com/action.php'
+    // api: 'https://nycferry.hialabs.com/action.php'
+    api: 'https://nycferry.hialabs.com/action3.php'
   }
 };
