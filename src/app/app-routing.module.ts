92e08f91cd5026a40f3c73f6d0754d872f4929e6
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-09 14:31:24 -0500

    Remove the paths with redirectTo to prevent the removal of query params in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { CrewboardComponent } from './crewboard/crewboard.component';
 import { MainComponent } from './main/main.component';
-import { CurrentAssignmentsComponent } from './assignments/current-assignments/current-assignments.component';
-import { PastAssignmentsComponent } from './assignments/past-assignments/past-assignments.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
 import { ManageDatabaseComponent } from './manage-database/manage-database.component';
 
 import { BasicTablesComponent } from './manage-database/basic-tables/basic-tables.component';
@@ -16,30 +15,24 @@ import { ShiftTableComponent } from './manage-database/shift-table/shift-table.c
 import { CrewmemberTableComponent } from './manage-database/crewmember-table/crewmember-table.component';
 import { CrewswapTableComponent } from './manage-database/crewswap-table/crewswap-table.component';
 
-const defaultPath = 'main/current-assignments';
-
 const routes: Routes = [
-  { path: '', redirectTo: defaultPath, pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'crewboard', component: CrewboardComponent },
-  { path: 'main', component: MainComponent, children: [
-    { path: '', redirectTo: defaultPath, pathMatch: 'full' },
-    { path: 'current-assignments', component: CurrentAssignmentsComponent },
-    { path: 'past-assignments', component: PastAssignmentsComponent },
+  { path: '', component: MainComponent, children: [
+    { path: '', component: AssignmentsComponent, pathMatch: 'full' },
     { path: 'manage-database', component: ManageDatabaseComponent, children: [
-      { path: '', redirectTo: '/main/manage-database/basic-tables', pathMatch: 'full' },
-      { path: 'basic-tables', component: BasicTablesComponent },
+      { path: '', component: BasicTablesComponent, pathMatch: 'full',  },
       { path: 'relationship-tables', component: RelationshipTablesComponent, children: [
-        { path: '', redirectTo: '/main/manage-database/relationship-tables/vessel', pathMatch: 'full' },
-        { path: 'vessel', component: VesselTableComponent },
+        { path: '', component: VesselTableComponent, pathMatch: 'full' },
         { path: 'shift', component: ShiftTableComponent },
         { path: 'crew-member', component: CrewmemberTableComponent },
         { path: 'crew-swap', component: CrewswapTableComponent }
       ] }
     ] }
   ] },
-  { path: '**', redirectTo: defaultPath }
+  { path: 'crewboard', component: CrewboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/' }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
