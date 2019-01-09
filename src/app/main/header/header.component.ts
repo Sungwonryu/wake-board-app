c15b6c427284a52f86ca730c23efb0fee4c4ebd5
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-09 14:03:31 -0500

    Add changeDate() and the related codes in header.component.ts
    . Change the links to 'assignments', 'crewboard' and 'manage database'

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,22 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+
+import { HDate } from '../../shared/lib/h-date';
+import { ParamsService } from '../../shared/services/params.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnChanges, OnInit {
+
+  HDate = HDate;
 
   navList = [
-    { routerLink: '/main/current-assignments', caption: `Current\nAssignments` },
-    { routerLink: '/main/past-assignments', caption: `Past\nAssignments` },
-    { routerLink: '/crewboard', caption: `Crewboard\nPreview` },
-    { routerLink: '/main/manage-database', caption: `Manage\nDatabase` }
+    { routerLink: '/', caption: `Assignments`, routerLinkActiveOptions: { exact: true } },
+    { routerLink: '/crewboard', caption: `Crewboard`, routerLinkActiveOptions: { exact: false } },
+    { routerLink: '/manage-database', caption: `Manage\nDatabase`, routerLinkActiveOptions: { exact: false } }
   ];
 
-  constructor() { }
+  @Input() hasDatePicker: boolean = false;
+  @Input() date: Date = null;
+  @Output() onChangeDate = new EventEmitter<Date>();
+
+  constructor(
+    private paramsService: ParamsService
+  ) { }
 
   ngOnInit() {
+    this.date = this.paramsService.getDate();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // When the changes on this.date happens
+    if (changes.date) {
+      console.log('changes on date, date: ', this.date);
+    }
+  }
+
+  changeDate(newDate: Date) {
+    this.onChangeDate.emit(newDate);
+  }
+
+  changeDateByOneDay(sign: string) {
+    const dayTime = 86400000;
+    let newDate;
+    switch(sign) {
+      case '+':
+        newDate = new Date(this.date.getTime() + dayTime);
+        break;
+      case '-':
+        newDate = new Date(this.date.getTime() - dayTime);
+        break;
+    }
+    if (newDate) {
+      this.changeDate(newDate);
+    }
+  }
+
+  openCalendarForm() {
+    alert('hi');
   }
 
 }
