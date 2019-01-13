f95d9c0409918b7dc17bd6a70b2ee2fe427fbe62
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-13 13:19:42 -0500

    Add date-form-dialog.component.ts

--- src/app/assignments/date-form-dialog/date-form-dialog.component.ts
+++ src/app/assignments/date-form-dialog/date-form-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-date-form-dialog',
@@ -6,10 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-form-dialog.component.scss']
 })
 export class DateFormDialogComponent implements OnInit {
+  @ViewChild('f') form: NgForm;
+  @ViewChild('datePicker') datePicker: any;
 
-  constructor() { }
+  formInitTimeoutId: any;
+  formInitInterval = 400; // Delay for this.form.setValue() after form init
+  unlockTimeoutId: any;
+  unlockInterval = 200; // Delay for this.noteService.api('unlock', this.item) after closing NoteFormDialogComponent
+
+  title = 'Calendar';
+  instruction = 'View a specific day’s Assignments using one or more of the fields below.';
+  subtitle1 = '';
+  subtitle2 = '';
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<DateFormDialogComponent>
+  ) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    // If this.timeoutId id present, clear the previously invoked setTimeout()
+    if (this.formInitTimeoutId !== null) {
+      clearTimeout(this.formInitTimeoutId);
+    }
+    this.formInitTimeoutId = setTimeout(() => {
+      this.setFormValue(this.data);
+      this.datePicker.opened = true;
+    }, this.formInitInterval);
+  }
+
+  setFormValue(data: any) {
+    this.form.setValue({
+      date: data.date
+    });
+  }
+
+  onCloseForm() {
+    this.dialogRef.close();
+  }
+
+  onSave() {
+    const newDate = this.form.value['date'];
+    this.dialogRef.close({ date: newDate });
   }
 
 }
