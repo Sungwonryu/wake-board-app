b92f4307238af58b5b17367d9525f76934d6486b
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-15 05:34:43 -0500

    Refactor CrewswapTableComponent and add the codes for opening CrewFormDialogComponent

--- src/app/manage-database/crewswap-table/crewswap-table.component.ts
+++ src/app/manage-database/crewswap-table/crewswap-table.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
 import { Subscription } from 'rxjs';
-import { HString } from '../../shared/lib/h-string';
 
+import { HString } from '../../shared/lib/h-string';
 import { ListUpdate } from '../../api-storage/api-storage.model';
-import { ShiftService } from '../shift-table/shift.service';
+import { TableActionData } from '../../shared/tables/table.model';
+
+import { CrewshiftService } from './crewshift.service';
+import { CrewFormDialogComponent } from '../crew-form-dialog/crew-form-dialog.component';
 
 @Component({
   selector: 'app-crewswap-table',
@@ -23,18 +27,20 @@ export class CrewswapTableComponent implements OnInit {
       { columnDef: 'modifyEntry', header: 'Modify Entry', width: '244px', isModifyEntry: true, modifyEntryButtons: ['edit', 'delete'] }
     ],
     tableTitle: 'CREW SWAP RELATIONSHIPS',
-    dataType: 'shifts'
+    dataType: 'crewshifts'
   };
 
-  shiftList: any[] = [];
-  $shiftListUpdateSub: Subscription;
+  list: any[] = [];
+  $listUpdateSub: Subscription;
+
+  formDialogRef: MatDialogRef<any>;
 
   constructor(
-    private shiftService: ShiftService
+    private dialog: MatDialog,
+    private mainService: CrewshiftService
   ) { }
 
   ngOnInit() {
-    console.log('VesselTableComponent is init - this: ', this);
     this.initListUpdate();
     this.initList();
     this.initService();
@@ -42,25 +48,64 @@ export class CrewswapTableComponent implements OnInit {
 
   initListUpdate() {
     // shift
-    this.$shiftListUpdateSub = this.shiftService.$listUpdate.subscribe((listUpdate: ListUpdate) => {
-      console.log(`${this.shiftService.object} - list is fetched`);
+    this.$listUpdateSub = this.mainService.$listUpdate.subscribe((listUpdate: ListUpdate) => {
+      console.log(`${this.mainService.object} - list is fetched`);
       if (listUpdate && listUpdate.isUpdated === true) {
-        this.shiftList = this.shiftService.getList();
+        this.list = this.mainService.getList();
       }
     });
   }
 
   initList() {
-    this.shiftList = this.shiftService.getList();
+    this.list = this.mainService.getList();
   }
 
   initService() {
-    this.shiftService.api('read');
+    this.mainService.api('read');
   }
 
   ngOnDestroy() {
-    if (this.$shiftListUpdateSub) {
-      this.$shiftListUpdateSub.unsubscribe();
+    if (this.$listUpdateSub) {
+      this.$listUpdateSub.unsubscribe();
+    }
+  }
+
+  openFormDialog(tableActionData: TableActionData, formDialogComponent: any, panelClass: string) {
+    this.formDialogRef = this.dialog.open(formDialogComponent, {
+      panelClass: panelClass,
+      // Disable the feature that the user can use escape or clicking outside to close a modal.
+      disableClose: true,
+      data: {
+        tableActionData: tableActionData
+      }
+    });
+
+    // After the formDialog is closed, refresh the lists
+    if (this.formDialogRef) {
+      this.formDialogRef.afterClosed().subscribe(() => {
+        this.initService();
+      });
+    }
+  }
+
+  modifyTable(tableActionData: TableActionData) {
+    let panelClass;
+    let formDialogComponent;
+    console.log('tableActionData: ', tableActionData);
+
+    switch(tableActionData.tableAction) {
+      case 'add':
+      case 'edit':
+        panelClass = 'relationship-form-dialog-container';
+        formDialogComponent = CrewFormDialogComponent;
+        if (tableActionData.tableAction === 'edit') {
+          this.mainService.api('override', tableActionData.entries[0]);
+        }
+        break;
+    }
+
+    if (panelClass && formDialogComponent) {
+      this.openFormDialog(tableActionData, formDialogComponent, panelClass);
     }
   }
 
