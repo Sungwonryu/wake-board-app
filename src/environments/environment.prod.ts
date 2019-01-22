ef8be0bfb538134ba4ffea906353ae71fb538016
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-22 11:47:37 -0500

    Change urls.api to https://nycferry.hialabs.com/action.php in environments/environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: false,
   urls: {
     userData: 'assets/data/user-data.json',
-    // api: 'https://nycferry.hialabs.com/action.php'
-    api: 'https://nycferry.hialabs.com/action3.php'
+    api: 'https://nycferry.hialabs.com/action.php'
   }
 };
