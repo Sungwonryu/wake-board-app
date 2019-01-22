ee89d34935ce5e5693aeaccbe33bd1bdd23bf2d8
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-01-22 11:57:27 -0500

    Add showAuthSnackbar() and modify showSnackbar() and showApiResponseMessage() in ui.service.ts

--- src/app/shared/services/ui.service.ts
+++ src/app/shared/services/ui.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { MatSnackBar } from '@angular/material';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
 
 import { ApiResponse } from '../../api-storage/api-storage.model';
 
@@ -11,6 +11,12 @@ export class UIService {
   shortSnackbarDuration = 800; // 800 millisecs
   actionsShowSuccessMessage = ['insert', 'update', 'delete', 'duplicate'];
   $loadingStateChange = new Subject<boolean>();
+
+  messageSucess = 'Success!';
+  messageFail = 'Fail!';
+  panelClass = ['snackbar-panel'];
+  panelClassSuccess = ['snackbar-panel-success'];
+  panelClassFail = ['snackbar-panel-fail'];
 
   constructor(
      private snackbar: MatSnackBar
@@ -24,48 +30,70 @@ export class UIService {
     this.$loadingStateChange.next(false);
   }
 
-  showSnackbar({ message, action, duration }: {
+  showSnackbar({ message, duration, action = null, panelClass = [] }: {
     message: string,
+    duration?: number,
     action?: string,
-    duration?: number
+    panelClass?: string[]
   }) {
+    const config = new MatSnackBarConfig();
+
+    // If panelClass is not valid, set config.panelClass as this.panelClass
+    if (panelClass && typeof panelClass === 'object' && panelClass.constructor === Array) {
+      config.panelClass = panelClass[0]
+    } else {
+      config.panelClass = this.panelClass;
+    }
+    // If duration is not valid, set config.duration as this.snackbarDuration
     if (duration || typeof duration !== 'number' || !Number.isInteger(duration)) {
-      // If duration is not valid, set duration as this.snackbarDuration
+      config.duration = this.snackbarDuration;
+    } else {
+      config.duration = duration;
+    }
+    this.snackbar.open(message, action, config);
+  }
+
+  showAuthSnackbar({ message, success = true }: { message: string, success: boolean }) {
+    let panelClass;
+    let duration;
+    if (success) {
+      panelClass = this.panelClassSuccess;
+      duration = this.shortSnackbarDuration;
+    } else {
+      panelClass = this.panelClassFail;
       duration = this.snackbarDuration;
     }
-    this.snackbar.open(message, action, {
+    this.showSnackbar({
+      message: message,
+      panelClass: panelClass,
       duration: duration
     });
   }
 
-  showApiResponseMessage({ apiOpts, success, error }: ApiResponse) {
+  showApiResponseMessage({ apiOpts, success }: ApiResponse) {
     let message: string;
-    let duration = this.snackbarDuration;
+    let duration: number = this.snackbarDuration;
+    let panelClass: string[] = this.panelClass;
 
     if (success) {
       // When ApiResponse is successful
       if (this.actionsShowSuccessMessage.indexOf(apiOpts.baseParamsObj.action) !== -1) {
         // apiOpts.baseParamsObj.action is found in this.actionsShowSuccessMessage
-        message = `"${apiOpts.baseParamsObj.object}" - ${apiOpts.baseParamsObj.action} operation was successful.`;
+        message = this.messageSucess;
+        panelClass = this.panelClassSuccess;
         duration = this.shortSnackbarDuration;
       }
     } else {
       // When ApiResponse is not successful
-      if (error) {
-        // When there was a network failure
-        message = `Sorry, there was a http failure on "${apiOpts.baseParamsObj.object}" ${apiOpts.baseParamsObj.action} operation.
-  Please try later.`;
-      } else {
-        // When there was no network error but API operation was not successful
-        message = `Sorry, there was a problem on "${apiOpts.baseParamsObj.object}" ${apiOpts.baseParamsObj.action} operation.
-  Please try later.`;
-      }
+      message = this.messageFail;
+      panelClass = this.panelClassFail;
     }
 
     if (message) {
       // When message is set
       this.showSnackbar({
         message: message,
+        panelClass: panelClass,
         duration: duration
       });
     }
