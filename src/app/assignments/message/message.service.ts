7f7bb06160b397974e5efa4d59f8240c47537c75
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-02-12 18:31:54 -0500

    Modify list in message.service.ts

--- src/app/assignments/message/message.service.ts
+++ src/app/assignments/message/message.service.ts
@@ -2,10 +2,10 @@ import { Message } from './message.model';
 
 export class MessageService  {
   list: Message[] = [
-    { message: 'See Port Captain', symbol: '?', abbreviation: 'PC' },
-    { message: 'See Human Resources', symbol: '!', abbreviation: 'HR' },
-    { message: 'Report to Pier 11', symbol: '*', abbreviation: '11' },
-    { message: 'Training Assignment', symbol: '^', abbreviation: 'T' }
+    { id: '1', message: 'See Port Captain', symbol: '?', abbreviation: 'PC' },
+    { id: '2', message: 'See Human Resources', symbol: '!', abbreviation: 'HR' },
+    { id: '3', message: 'Report to Pier 11', symbol: '*', abbreviation: '11' },
+    { id: '4', message: 'Training Assignment', symbol: '^', abbreviation: 'T' }
   ];
 
   constructor() { }
