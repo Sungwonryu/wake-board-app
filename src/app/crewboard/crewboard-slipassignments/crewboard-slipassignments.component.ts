ee77545960de3c1c7d2c6846d9c228c8a3d71d6b
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-05-23 15:39:18 -0400

    Change slipList in crewboard-slipassignments.component.ts

--- src/app/crewboard/crewboard-slipassignments/crewboard-slipassignments.component.ts
+++ src/app/crewboard/crewboard-slipassignments/crewboard-slipassignments.component.ts
@@ -1,3 +1,51 @@
+// import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+//
+// import { VesselService } from '../../manage-database/vessel-table/vessel.service';
+//
+// @Component({
+//   selector: 'app-crewboard-slipassignments',
+//   templateUrl: './crewboard-slipassignments.component.html',
+//   styleUrls: ['./crewboard-slipassignments.component.scss']
+// })
+// export class CrewboardSlipassignmentsComponent implements OnDestroy, OnInit {
+//
+//   @Input() data: any[] = [];
+//
+//   slipList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+//   visibleTableId: 0 | 1 = 0;
+//   changeTableDelay = 20000; // 20000 ms = 20 sec
+//   intervalId: any = null;
+//
+//   constructor(
+//     public vesselService: VesselService
+//   ) { }
+//
+//   ngOnInit() {
+//     this.intervalId = setInterval(() => {
+//       this.changeVisibleTable();
+//     }, this.changeTableDelay);
+//   }
+//
+//   ngOnDestroy() {
+//     if (this.intervalId) {
+//       clearInterval(this.intervalId);
+//     }
+//   }
+//
+//   getPropFromData(slip: string, prop: string) {
+//     let matchedProp;
+//     let matchedItem = this.data.find((item: any) => item.slip === slip);
+//     if (matchedItem) {
+//       matchedProp = matchedItem[prop];
+//     }
+//     return matchedProp;
+//   }
+//
+//   changeVisibleTable() {
+//     this.visibleTableId = (this.visibleTableId === 0) ? 1 : 0;
+//   }
+// }
+
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 import { VesselService } from '../../manage-database/vessel-table/vessel.service';
@@ -11,7 +59,18 @@ export class CrewboardSlipassignmentsComponent implements OnDestroy, OnInit {
 
   @Input() data: any[] = [];
 
-  slipList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  slipList = [
+    ['1', '6', '11'],
+    ['2', '7', '12'],
+    ['3', '8', '13'],
+    ['4', '9', '14'],
+    ['5', '10', '15'],
+    ['16', '21', '26'],
+    ['17', '22', '27'],
+    ['18', '23', '28'],
+    ['19', '24', '29'],
+    ['20', '25', '30']
+  ];
   visibleTableId: 0 | 1 = 0;
   changeTableDelay = 20000; // 20000 ms = 20 sec
   intervalId: any = null;
