6651b5a97476e176bd4831e122a2a5df9425df3f
Sungwon Ryu <sungwon@syllabussoftware.org>
2019-05-23 15:47:33 -0400

    Change the model of Slipassignment in slipassignment.model.ts

--- src/app/assignments/slipassignment/slipassignment.model.ts
+++ src/app/assignments/slipassignment/slipassignment.model.ts
@@ -4,28 +4,40 @@ import { BaseApiObj, BaseDataObj } from '../../api-storage/base-data';
  *  SlipassignmentApiObj is the interface of returning objects from API
  *  SlipassignmentApiObj interface uses underscore case for its property names
 */
+// export interface SlipassignmentApiObj extends BaseApiObj {
+//   'date'?: string | null;
+//   'slip'?: string | null;
+//   'vessel1_id'?: string | null;
+//   'vessel2_id'?: string | null;
+//   'vessel3_id'?: string | null;
+//   'vessel1_availability'?: '0' | '1' | null;
+//   'vessel2_availability'?: '0' | '1' | null;
+//   'vessel3_availability'?: '0' | '1' | null;
+// }
 export interface SlipassignmentApiObj extends BaseApiObj {
   'date'?: string | null;
   'slip'?: string | null;
-  'vessel1_id'?: string | null;
-  'vessel2_id'?: string | null;
-  'vessel3_id'?: string | null;
-  'vessel1_availability'?: '0' | '1' | null;
-  'vessel2_availability'?: '0' | '1' | null;
-  'vessel3_availability'?: '0' | '1' | null;
+  'vessel_id'?: string | null;
+  'vessel_availability'?: '0' | '1' | null;
 }
 
 /**
  *  Slipassignment is the base interface for the objects used in the front-end
  *  Slipassignment interface uses underscore case for its property names
 */
+// export interface Slipassignment extends BaseDataObj {
+//   date?: Date | null;
+//   slip?: string | null;
+//   vessel1Id?: string | null;
+//   vessel2Id?: string | null;
+//   vessel3Id?: string | null;
+//   vessel1Availability?: '0' | '1' | null;
+//   vessel2Availability?: '0' | '1' | null;
+//   vessel3Availability?: '0' | '1' | null;
+// }
 export interface Slipassignment extends BaseDataObj {
   date?: Date | null;
   slip?: string | null;
-  vessel1Id?: string | null;
-  vessel2Id?: string | null;
-  vessel3Id?: string | null;
-  vessel1Availability?: '0' | '1' | null;
-  vessel2Availability?: '0' | '1' | null;
-  vessel3Availability?: '0' | '1' | null;
+  vesselId?: string | null;
+  vesselAvailability?: '0' | '1' | null;
 }
